6b19c2c6f2ac9fc62453b2c79be18cdbefb0632c
Shariea Reza Nabil <nabilreza183@gmail.com>
2026-03-08 07:49:25 +0600

    sign in functional done

---
+++ sign.js
@@ -0,0 +1,17 @@
+const signBtn = document.getElementById('sign-btn');
+const userName = document.getElementById('user-name');
+const userPassword = document.getElementById('user-password');
+
+signBtn.addEventListener('click', () => {
+    const userNameValue = userName.value;
+    const userPasswordValue = userPassword.value;
+
+    if (userNameValue === 'admin' && userPasswordValue === 'admin123') {
+        alert('Sign In Success!');
+        window.location.assign('/home.html');
+    } else {
+        alert('Sign In Failed');
+        return;
+    }
+    
+});
